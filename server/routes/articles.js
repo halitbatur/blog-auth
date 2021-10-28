fc11d208511600fd7e2139586d7ba01b953f95d4
Wisam Naji <guitar90rocker@gmail.com>
2021-10-28 00:58:58 +0000

    styling tweaks

--- server/routes/articles.js
+++ server/routes/articles.js
@@ -44,12 +44,14 @@ function saveArticleAndRedirect(path) {
   return async (req, res) => {
     let article = req.article;
     article.title = req.body.title;
-    article.description = req.body.description;
+    article.snippet = req.body.snippet;
     article.markdown = req.body.markdown;
+    article.author = req.user._id;
     try {
       article = await article.save();
       res.redirect(`/articles/${article.slug}`);
     } catch (e) {
+      console.log(e);
       res.render(`articles/${path}`, { article: article });
     }
   };
