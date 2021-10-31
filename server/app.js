4889c76fa1cc9b9c8b0f856b8e975233ef732eb2
Wisam Naji <guitar90rocker@gmail.com>
2021-10-31 01:00:39 +0000

    added sign in and up pages

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const partials = require('express-partials');
 const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
 const path = require('path');
+const fs = require('fs');
+const crypto = require('crypto');
 const Article = require('./models/article');
 const articleRouter = require('./routes/articles');
 const authRouter = require('./routes/auth');
@@ -28,7 +31,45 @@ app.get('/', async (req, res) => {
 });
 
 app.use('/articles', articleRouter);
-app.use('/auth', authRouter);
+app.use('/user', authRouter);
+
+// Avatar upload and fetch
+app.use(fileUpload({ createParentPath: true }));
+app.post('/avatar', function (req, res) {
+  let sampleFile, uploadPath, ext;
+
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  sampleFile = req.files.file;
+  if (!sampleFile.mimetype.startsWith('image/')) {
+    return res.status(401).send('Wrong file type. Only images are accepted');
+  } else if (sampleFile.size > 550 * 1024) {
+    return res.status(401).send('File is too big. Max size is 550 kb');
+  }
+
+  ext = `.${sampleFile.name.split('.').pop()}`;
+
+  let name = crypto.randomUUID() + ext;
+  uploadPath = `${__dirname}/../__cache/${name}`;
+
+  // Use the mv() method to place the file somewhere on your server
+  sampleFile.mv(uploadPath, function (err) {
+    if (err) return res.status(500).send(err);
+
+    res.json({ success: true, url: `/avatar/${name}` });
+  });
+});
+
+app.get('/avatar/:file', (req, res) => {
+  let path = `${__dirname}/../__cache/${req.params.file}`;
+  let exists = fs.existsSync(path);
+  if (exists) {
+    fs.createReadStream(path).pipe(res);
+  } else res.status(404).send('not found');
+});
 
 const PORT = process.env.SERVER_PORT || 8080;
 app.listen(PORT, () => {
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,6 +2,39 @@ const express = require('express');
 const User = require('./../models/user');
 const router = express.Router();
 
-// define your routes here
+// @TODO: Assignment here
+
+router.post('/signin', (req, res) => {
+  const { username, password, rememberMe } = req.body;
+  // @TODO: Complete user sign in
+});
+
+router.post('/signup', (req, res) => {
+  const {
+    firstname,
+    lastname,
+    username,
+    password,
+    password2,
+    acceptTos, // either "on" or undefined
+    avatar,
+  } = req.body;
+
+  // @TODO: Complete user sign up
+});
+
+router.get('/signout', (req, res) => {
+  // @TODO: Complete user sign out
+});
+
+// renders sign up page
+router.get('/signup', (req, res) => {
+  res.render('user/signup');
+});
+
+// renders sign in page
+router.get('/signin', (req, res) => {
+  res.render('user/signin');
+});
 
 module.exports = router;
